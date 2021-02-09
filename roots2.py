b4cf3372644a41135530f06926326813e8d3f641
Damian Griffiths <damiangriffiths81@gmail.com>
2021-02-09 11:03:32 +0000

    python changes

--- roots2.py
+++ roots2.py
@@ -1,10 +1,11 @@
-#!/usr/bin/pypy
+#!/usr/bin/python3
 import math
 
 MaxRoots = 10000000
+roots = []
 
 #Main program
-for i in range (0,MaxRoots):
+for i in range (0,MaxRoots+1):
     roots += [math.sqrt(i+1)]
 #    print("Root: %d:%8.2f" % (i,roots[i]))
 	
